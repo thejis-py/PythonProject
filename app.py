57af132688e1212d479eaf4b709d496e93c5f0e3
thejis-py <jis.kanyanat@gmail.com>
2023-11-13 00:42:36 +0700

    deal with sqlalchemy, sell+buy+history

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_session import Session as FlaskSession
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import lookup
+from datetime import datetime
 
 #api=DM8J043TJV3OYW8H
 app = Flask(__name__)
@@ -16,17 +17,54 @@ db = SQLAlchemy(app)
 
 #users table
 class Users(db.Model):
+      #__tablename__ = "users"
       _id = db.Column(db.Integer, primary_key=True)
-      _username = db.Column(db.String(50), unique=True)
-      _hash = db.Column(db.String())
-      _cash = db.Column(db.Float)
+      username = db.Column(db.String(50), unique=True)
+      hash = db.Column(db.String())
+      cash = db.Column(db.Float)
 
       def __init__(self, username, hash, cash=10000):
-            self._username = username
-            self._hash = hash
-            self._cash = cash
+            self.username = username
+            self.hash = hash
+            self.cash = cash
       def __repr__(self):
-            return f"({self._id}) {self._username} {self._hash} {self._cash}"
+            return f"({self._id}) {self.username} {self.hash} {self.cash}"
+class Portfolio(db.Model):
+      #__tablename__ = "porfolio"
+
+      _id = db.Column(db.Integer, primary_key=True)
+      symbol = db.Column(db.String(50))
+      amount = db.Column(db.Integer)
+      purchase_price = db.Column(db.Float)
+      user_id = db.Column(db.Integer)
+
+      def __init__(self, symbol, amount, purchase_price, user_id):
+            self.symbol = symbol
+            self.amount = amount
+            self.purchase_price = purchase_price
+            self.user_id = user_id
+      def __repr__(self):
+            return f"({self._id}) {self.symbol} {self.amount} {self.purchase_price} {self.user_id}"
+class History(db.Model):
+      _id = db.Column(db.Integer, primary_key=True)
+      side = db.Column(db.String)
+      symbol = db.Column(db.String(50))
+      price = db.Column(db.Float)
+      amount = db.Column(db.Integer)
+      time = db.Column(db.DateTime)
+      user_id = db.Column(db.Integer)
+
+      def __init__(self, side, symbol, price, amount, user_id):
+            self.side = side
+            self.symbol = symbol
+            self.price = price
+            self.amount = amount
+            self.time = datetime.now()
+            self.user_id = user_id
+
+#create table if not exist
+with app.app_context():
+      db.create_all()
 
 
 #print data
@@ -37,6 +75,17 @@ with app.app_context():
       print("testt")
       for i in users:
             print(i)
+      port = Portfolio.query.all()
+      db.session.commit()
+      print("testt poooort")
+      for i in port:
+            print(i)
+      history = History.query.all()
+      db.session.commit()
+      print("inhis")
+      for i in history:
+            print("his",i)
+
 
 #config session
 app.config["SESSION_PERMANENT"] = False
@@ -44,12 +93,68 @@ app.config["SESSION_TYPE"] = "filesystem"
 FlaskSession(app)
 
 
+@app.route("/history")
+def history():
+      history = History.query.all()
+      db.session.commit()
+      print("inhis")
+      for i in history:
+            print("his",i)
+      return render_template("history.html", history = History.query.all())
+@app.route("/sell", methods=["POST", "GET"])
+def sell():
+      if request.method == "POST":
+            symbol = request.form.get("symbol").upper()
+            amount = int(request.form.get("amount"))
+            with app.app_context():
+                  history = History("Sell", symbol, lookup(symbol)["price"], amount, session["user_id"])
+                  db.session.add(history)
+                  
+                  found_symbol = Portfolio.query.filter_by(user_id=session["user_id"], symbol=symbol).first()
+                  found_symbol.purchase_price -= amount*(found_symbol.purchase_price/found_symbol.amount)
+                  found_symbol.amount -= amount
+
+
+                  db.session.commit()
+                  return redirect("/")
+      return render_template("sell.html")
+@app.route("/buy", methods=["POST", "GET"])
+def buy():
+      if request.method == "POST":
+            symbol = request.form.get("symbol").upper()
+            amount = int(request.form.get("amount"))
+            with app.app_context():
+                  history = History("Buy", symbol, lookup(symbol)["price"], amount, session["user_id"])
+                  db.session.add(history)
+
+                  found_symbol = Portfolio.query.filter_by(user_id=session["user_id"], symbol=symbol).first()
+                  if not found_symbol:
+                        new_port = Portfolio(symbol, amount, round(float(lookup(symbol)["price"])*amount, 2), session["user_id"])
+                        db.session.add(new_port)
+                        db.session.commit()
+                        return redirect("/")
+                  found_symbol.purchase_price += round(lookup(symbol)["price"]*amount, 2)
+                  found_symbol.amount += amount
+
+
+                  db.session.commit()
+
+                  return redirect("/")
+      return render_template("buy.html")
+
 @app.route("/", methods=["POST", "GET"])
 def home():
       if request.method=="POST":
-            print("query", request.form.get("query"))
             return render_template("quoted.html", data = lookup(request.form.get("query")))
-      return render_template("index.html")
+
+      data = []
+
+      user_port = Portfolio.query.filter_by(user_id = session["user_id"])
+      for i in user_port:
+            print(i)
+            data.append([i.symbol, lookup(i.symbol)["price"], i.purchase_price, i.amount])
+
+      return render_template("index.html", data=data)
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
@@ -62,10 +167,9 @@ def register():
            if not password:
                  return render_template("register.html", error_m = "Please provide password")
 
-           found_user = Users.query.filter_by(_username=username).first()
+           found_user = Users.query.filter_by(username=username).first()
 
            if found_user:
-                 print("-----found_user",found_user._id)
                  return render_template("register.html", error_m = "Username is already exist")
            if password != password_conf:
                  return render_template("register.html", error_m = "Password not match")
@@ -74,7 +178,8 @@ def register():
            with app.app_context():
                   db.session.add(new_user)
                   db.session.commit()
-           session["user_id"] = Users.query.filter_by(_username=username).first()._id
+
+           session["user_id"] = Users.query.filter_by(username=username).first()._id
 
            return redirect("/")
      return render_template("register.html")
@@ -90,10 +195,11 @@ def login():
                   return render_template("login.html", error_m = "Please provide username")
             if not password:
                   return render_template("login.html", error_m = "Please provide password")
-            found_user = Users.query.filter_by(_username=username).first()
+
+            found_user = Users.query.filter_by(username=username).first()
             if not found_user:
                   return render_template("login.html", error_m="Username not found")
-            if not check_password_hash(found_user._hash, password):
+            if not check_password_hash(found_user.hash, password):
                   return render_template("login.html", error_m="Incorrect Password")
             session["user_id"] = found_user._id
             return redirect("/")
@@ -107,7 +213,5 @@ def logout():
       return redirect("/")
 
 if __name__ == "__main__":
-      with app.app_context():
-            db.create_all()
       app.run()
 
